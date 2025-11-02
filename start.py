9353440c6a5adf7892b3b5b3613339d86a3f5bd9
whiskey <634772805@qq.com>
2025-11-02 16:01:50 +0800

    commit_3

---
+++ start.py
@@ -0,0 +1,14 @@
+# hello world program in my python learning
+print('hello python')
+# print()方法会依次打印每个字符串,遇到英文逗号,会输出一个空格
+print('hello','world')
+# print()方法也可以打印整数或者计算结果
+print(1096)
+print('100+200 = ',100+200)
+
+# 输入
+print('请在控制台输入字符串或数字,输入完成后单击回车键完成')
+a = input()
+print('你刚刚输入了:',a)
+
+# python中的变量
